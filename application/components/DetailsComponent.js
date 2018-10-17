63949c96a0637d4d83079b21ba0919bd96001cd8
Luca Ford <lucasubiranaford@gmail.com>
2018-10-17 15:39:23 -0300

    Cambiasos x2
    Cambiasos

--- application/components/DetailsComponent.js
+++ application/components/DetailsComponent.js
@@ -2,17 +2,52 @@ import React, {Component} from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image,
+    TouchableHighlight,
+    TouchableWithoutFeedback,
+    ScrollView,
+    ImageBackground,
+    Dimensions
 } from 'react-native';
+
+const {width, height} = Dimensions.get('window');
 
 class Details extends Component {
     render(){
+        const {thumbnail} = this.props.item.details
         return(
-            <View>
-                <Text>Details</Text>
-            </View>
+            <ScrollView style={styles.container}>
+                <ImageBackground
+                    source={{uri: thumbnail}}
+                    style={styles.thumbnail}
+                >
+                    <TouchableWithoutFeedback>
+                        <View>
+                            <Image
+                                source={require('../images/icnPlay.jpg')}
+                                style={styles.imagePlay}
+                            />
+                        </View>
+                    </TouchableWithoutFeedback>
+                </ImageBackground>
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    thumbnail: {
+        width: width,
+        height: 300
+    },
+    imagePlay: {
+        height: 30,
+        width: 30
+    }
+})
 
 export default Details;
